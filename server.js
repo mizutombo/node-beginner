e438af14480353c953f303e95c130c7f424f0c84
mizutombo <thomas.shultz52@gmail.com>
2017-02-12 23:18:12 -0800

    debugs refactored server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var http = require('http');
 var url = require('url');
 
 function start(route, handle) {
-	function onRequest(req, res) {
-		var pathname = url.parse(req.url).pathname;
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
 		console.log('Request for ' + pathname + 'received.');
 
 		route(handle, pathname, response);
